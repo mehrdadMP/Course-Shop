a6578a582075b02b23221530f2bbbcf5ce6a37c5
Mehrdad MoienPour <mehrdad74mp@gmail.com>
2023-10-15 16:42:15 +0330

    git_test.dart modified and added to new test branch repo

---
+++ git_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class TestWidget extends StatelessWidget {
+  const TestWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
