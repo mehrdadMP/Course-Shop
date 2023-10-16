4814a366c642f27598d90b41fc9369b1140e5843
Mehrdad MoienPour <mehrdad74mp@gmail.com>
2023-10-16 20:19:54 +0330

    test1

--- lib/cart_screen.dart
+++ lib/cart_screen.dart
@@ -7,7 +7,7 @@ class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.amber,
+      color: Colors.blue,
       margin: EdgeInsets.all(20),
     );
   }
